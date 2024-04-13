9ff3c93d488aa694f1c78b1d182a3239dabc17ef
markcunn <markcunn@hotmail.com>
2024-04-13 10:46:06 +0000

    Lab 1 completed

--- main.cpp
+++ main.cpp
@@ -1,23 +1,35 @@
 #include "mbed.h"
 #include <cstdio>
 
-int t1 = 0;
-int t2 = 1;
-int next_term = t1 + t2;
+
 int max_terms = 10;
 
-    //0, 1, 1, 2, 3, 5, 8, 13, 21...
+//fibonacci sequence  0, 1, 1, 2, 3, 5, 8, 13, 21...
+// n = 0, ans = 0
+// n = 1, ans = 1
+// n = 2, ans = (0+1) = 1
+// n = 3, ans = (1+1) = 2
+// and so on
+
+int fib(int n){
+    
+    // n is the nth term result
+
+    if(n == 0 || n == 1)
+        return n;
+    else
+        return (fib(n-1) + fib(n-2));
+
+}
 
 int main()
 {
-    printf("Hello, lab program 1 \r\n");
-    printf("Fibonacci sequence: %d, %d, ", t1, t2);
-
-        for(int i=3; i<=max_terms; i++){
-            printf("%d, ", next_term);
-                t1 = t2;
-                t2 = next_term;
-                next_term = t1 + t2;
+    //main function of program
+    printf("Hello, lab program 1 using recursive functions\r\n");
+   
+        for(int i=0; i<max_terms; i++){
+            printf("%d, ", fib(i));
+                              
             }
 
     printf ("\r\n");
