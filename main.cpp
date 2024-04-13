badd76d9b70f396e897eeefc43fc980895636a4c
markcunn <markcunn@hotmail.com>
2024-04-13 10:23:17 +0000

    Fibonacci series

--- main.cpp
+++ main.cpp
@@ -1,10 +1,27 @@
 #include "mbed.h"
+#include <cstdio>
 
-//a second commit
+int t1 = 0;
+int t2 = 1;
+int next_term = t1 + t2;
+int max_terms = 10;
 
-// main() runs in its own thread in the OS
+    //0, 1, 1, 2, 3, 5, 8, 13, 21...
+
 int main()
 {
+    printf("Hello, lab program 1 \r\n");
+    printf("Fibonacci sequence: %d, %d, ", t1, t2);
+
+        for(int i=3; i<=max_terms; i++){
+            printf("%d, ", next_term);
+                t1 = t2;
+                t2 = next_term;
+                next_term = t1 + t2;
+            }
+
+    printf ("\r\n");
+
     while (true) {
 
     }
